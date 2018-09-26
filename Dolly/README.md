e35e57b6a71d5c647fc73542e0fef1ad76630bb0
oizovita <oles1103@gmail.com>
2018-09-26 16:07:55 +0300

    fixed creation of pdf files via html template

--- Dolly/README.md
+++ Dolly/README.md
@@ -1,5 +1,7 @@
-The script parses the excel table by selecting the column names and their meanings from it.
-Then substitutes it in a template and stores new instances in the specified folder
-The number of instances corresponds to the number of rows in the table
-To run the script from the command line, type 
- dolly.py [-h] [-t TYPE_FILE] [-f JSON_FILE] data template output_folder
+The script analyzes the excel table, selecting from it the column names and their values.
+Then replaces it in the template and stores the new instances in the specified folder
+To create Word documents, you need to use the docx templates
+If you need PDF files use the html template
+The number of instances corresponds to the number of rows in the table.
+To run the script from the command line, type
+dolly.py [-h] [-t TYPE_FILE] data template output_folder
--- Dolly/dolly.py
+++ Dolly/dolly.py
@@ -1,16 +1,13 @@
 import os
-import json
-import pdfrw
+import pdfkit
 import argparse
 from xlrd import open_workbook
 from docxtpl import DocxTemplate
+from jinja2 import FileSystemLoader, Environment
 
+FILE_HTML = 'html'
 FILE_DOCX = 'docx'
-FILE_PDF = 'pdf'
-ANNOT_KEY = '/Annots'
-ANNOT_FIELD_KEY = '/T'
-SUBTYPE_KEY = '/Subtype'
-WIDGET_SUBTYPE_KEY = '/Widget'
+TMP_HTML_TEMPLATE = 'template.html'
 
 
 # Checks if the folder already exists
@@ -53,47 +50,35 @@ def parser_excel_file(number):
 
 # Substitute the value from the dictionary into
 # the tamplate and create as many records as the rows
-
-def substitution_into_a_template(template, output_folder):
-    if template[-3:] == FILE_PDF:
-        print('Inavalif file format')
+def substitution_into_a_template(path_to_the_template, output_folder):
+    if path_to_the_template[-4:] == FILE_HTML:
+        print('Invalid file format')
         exit()
     for i in range(1, len(sheet.col(0))):
-        doc = DocxTemplate(template)
+        doc = DocxTemplate(path_to_the_template)
         doc.render(parser_excel_file(i))
         doc.save(output_folder + '/Invoice{0}.docx'.format(i))
 
 
-# Reads a pdf-file, searches in it for the same keys from the file
-# and dictionary and replaces the values in the file. Saves a new file
-def record_in_pdf_template(template, output_folder, data_dict):
-    if template[-4:] == FILE_DOCX:
-        print('Inavalif file format')
+# Looks for the path to the template. Enters the necessary data into a template
+# and saves it in a temporary  html file, then converts it into a PDF
+def create_pdf_from_html_template(path_to_the_template, output_folder):
+    if path_to_the_template[-4:] == FILE_DOCX:
+        print('Invalid file format')
         exit()
-    template_pdf = pdfrw.PdfReader(template)
-    annotations = template_pdf.pages[0][ANNOT_KEY]
-    for annotation in annotations:
-        if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-            if annotation[ANNOT_FIELD_KEY]:
-                key = annotation[ANNOT_FIELD_KEY][1:-1]
-                if key in data_dict.keys():
-                    annotation.update(pdfrw.PdfDict(V='{}'.format(data_dict[key])))
 
-    pdfrw.PdfWriter().write(output_folder, template_pdf)
+    template_html_file = path_to_the_template.split("/")[-1]
+    path_to_the_template = path_to_the_template[:-len(template_html_file)]
+    template = Environment(loader=FileSystemLoader(searchpath=path_to_the_template)).get_template(template_html_file)
 
-
-# Reads the json file by replacing the values in the dictionary
-# and passes it to record_in_pdf_template
-def create_pdf_templates(template, output_folder, json_file):
-    data_json = json.load(open(json_file))
     for i in range(1, len(sheet.col(0))):
-        data_dict = {}
-        for data in data_json:
-            for exel_row in parser_excel_file(i):
-                if data_json[data] == exel_row:
-                    data_dict[data] = parser_excel_file(i)[exel_row]
+        outputText = template.render(parser_excel_file(i))
+        html_file = open(TMP_HTML_TEMPLATE, 'w')
+        html_file.write(outputText)
+        html_file.close()
+        pdfkit.from_file('template.html', output_folder + '/Invoice{0}.pdf'.format(i))
 
-        record_in_pdf_template(template, output_folder + '/Invoice{0}.pdf'.format(i), data_dict)
+    os.remove(TMP_HTML_TEMPLATE)
 
 
 if __name__ == '__main__':
@@ -102,7 +87,7 @@ if __name__ == '__main__':
     parser.add_argument('data', action="store", help="path to the data file")
     parser.add_argument('template', action="store", help="path to the template file")
     parser.add_argument('output_folder', action="store", help="folder path with output")
-    parser.add_argument('-f', action="store", default="", dest="json_file", help="path to the json file")
+
     args = parser.parse_args()
 
     verification_of_paths(args.data, args.template)
@@ -111,15 +96,12 @@ if __name__ == '__main__':
     sheet = book.sheet_by_name(book.sheet_names()[0])
 
     create_folder(args.output_folder)
-
     if args.type_file is None:
         print('Enter the file type')
         exit()
     elif args.type_file == 'd':
         substitution_into_a_template(args.template, args.output_folder)
     elif args.type_file == 'p':
-        if not os.path.exists(args.json_file):
-            print("Path not found")
-            exit()
-
-        create_pdf_templates(args.template, args.output_folder, args.json_file)
+        create_pdf_from_html_template(args.template, args.output_folder)
+    else:
+        print('Incorrect key for document type entered')
